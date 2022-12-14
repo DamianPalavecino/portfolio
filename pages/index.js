814baf6410a5ce289af5af6500f88900d1cddd38
Damian Palavecino <damianpalavecino25@gmail.com>
2022-12-13 21:32:00 -0300

    Install Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      Welcome to my portfolio
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
